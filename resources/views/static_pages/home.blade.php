a0c1f12ce088f8ed2c0273249f7429cdd4b0aaab
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-03 06:52:27 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">
+    what you see is <a href="#">Laravel 教程</a>
+  </p>
+  <p>
+    Everything will begin from here!
+  </p>
+  <p>
+    <a href="#" class="btn btn-lg btn-success" role="button">Register now!</a>
+  </p>
+  <p>
+    <a class="btn btn-lg btn-success" href="{{ route('signup')}}" role="button">Sign up now!</a>
+  </p>
+</div>
 @stop
