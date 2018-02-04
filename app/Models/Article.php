dbbfd38118e0a91b3af1c8246c4d9b41cbbe0899
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-04 07:40:43 +0000

    Finish user signup

---
+++ app/Models/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //for change default table name from articles
+    //protected $table = 'other_table_name'
+}
