53bb03189fd56d2fc882868cbe135051dba486a9
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-04 08:08:25 +0000

    2nd update db config

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-public function get_db_config()
+function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
@@ -8,17 +8,17 @@ public function get_db_config()
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
-            'database' => substr($url["path"], 1),
-            'username' => $url["user"],
-            'password' => $url["pass"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
     } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-        ]
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
     }
 }
