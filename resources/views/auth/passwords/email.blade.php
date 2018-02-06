57ebfbd66fbaf6a72d3ce4100531868a33e15a37
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-06 02:46:14 +0000

    Add password resets & email configuration

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+@section('title', 'reset password')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Reset Password
+                </div>
+                <div class="panel-body">
+                    @if(session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <form class="form-horizontal" action="{{ route('password.email')}}" method="post">
+                        {{ csrf_field()}}
+                        <div class="form-group{{ $errors->has('email')? 'has-error': ''}}">
+                            <label for="email" class="col-md-4 control-label">email address</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email')}}" required>
+                                @if($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    send password reset email
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
